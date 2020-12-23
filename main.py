73a16595b86f7fbceda0a78df2f28092c80db357
Rayhan Mahmud <mahmudrayhan029@gmail.com>
2020-12-23 12:36:02 +0600

    user confirmation feature added

--- main.py
+++ main.py
@@ -10,13 +10,27 @@ def query_func(word):
     return data[word]
   
   elif len(get_close_matches(word, data.keys())) > 0 :
-    return f"{word} is dosen't exits. Do you mean %s "  % get_close_matches(word, data.keys())[0]
+    yn = input(f"'{word}' is dosen't exits. Do you mean '%s'. y/n?: "% get_close_matches(word, data.keys())[0])
+
+    if yn == 'y':
+      return data[get_close_matches(word, data.keys())[0]]
+    
+    elif yn == 'n':
+      return "The word doesn't exists."
+    else:
+      return "Sorry, we didn't understand your entry."
+
+
   elif word == "0000":
     print("You are now quiting.. \n ***THANKS FOR USING MY APPLICATION***")
     quit()
     
   else:
     return "The word doesn't exists. Please try again!"
+
+
+
+    
 i = 0
 while True:
   if i == 0:
@@ -26,7 +40,18 @@ while True:
   word_capitaliza = word.lower()
   print(query_func(word_capitaliza))
  
-  
+
+
+
+
+
+
+
+
+
+
+
+
   # # elif word in data:
   # #   return data[word]
   
