680e02cead6bbbaca7247ad6fb6ec8d0052413de
Rayhan Mahmud <mahmudrayhan029@gmail.com>
2020-12-24 12:47:10 +0600

    insert voice in main python file

---
+++ ex.py
@@ -0,0 +1,4 @@
+import json
+data2 = json.load(open('custom_data.json'))
+
+print(data2['name'])
--- main.py
+++ main.py
@@ -2,41 +2,78 @@ import json
 import difflib
 from difflib import get_close_matches 
 from difflib import SequenceMatcher
+import pyttsx3
 data = json.load(open("data.json"))
 
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):
+  engine.say(audio)
+  engine.runAndWait()
 
 def query_func(word):
   if word in data:
-    return data[word]
+    datas = data[word]
+    for item in datas:
+      speak(item)
+      print(item)
+    
+    
+  
+  elif word.upper() in data:
+    return data[word.upper()]
   
   elif len(get_close_matches(word, data.keys())) > 0 :
-    yn = input(f"'{word}' is dosen't exits. Do you mean '%s'. y/n?: "% get_close_matches(word, data.keys())[0])
+    speak(f"'{word}' is dosen't exists. Do you mean '%s'. Type y if it yes , type n if it no"% get_close_matches(word, data.keys())[0])
+    yn = input(f"'{word}' is dosen't exists. Do you mean '%s'. y/n?: "% get_close_matches(word, data.keys())[0])
+  
 
     if yn == 'y':
-      return data[get_close_matches(word, data.keys())[0]]
+      datas = (data[get_close_matches(word, data.keys())[0]])
+      for item in datas:
+        speak(item)
+        print(item)
+     
     
     elif yn == 'n':
+      speak("the word doesn't exists")
       return "The word doesn't exists."
-    else:
-      return "Sorry, we didn't understand your entry."
 
+    else:
+      speak("Sorry, we didn't understand your entry")
+      return "Sorry, we didn't understand your entry."
+     
 
   elif word == "0000":
+    speak("You are now quiting..THANKS FOR USING MY APPLICATION")
     print("You are now quiting.. \n ***THANKS FOR USING MY APPLICATION***")
     quit()
     
   else:
-    return "The word doesn't exists. Please try again!"
-
-
-
+    speak("The word doesn't exists. Please Please double check it.")
+    return "The word doesn't exists. Please Please double check it."
     
+
+
+
+
 i = 0
 while True:
   if i == 0:
+    speak("This is a word queris application. By typing a word you will able to know something about the word")
     print("***/This is a word queris application. By typing a word you will able to know something about the word/***\n -----THIS APPLICATION PROGRAMMED BY RAYHAN MAHMUD-----\n #####To exit type '0000'")
+    speak("THIS APPLICATION PROGRAMMED BY RAYHAN MAHMUD")
+    speak("To exit type 0000")
+   
+    
     i = i + 1
+  speak("Enter the word")
   word = input("Enter The Word: ")
+  if word != "0000":
+    speak("The meanings of the word")
+    speak(word)
   word_capitaliza = word.lower()
   print(query_func(word_capitaliza))
  
