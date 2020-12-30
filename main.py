0e038b39298f93960b630c3095f0284cf9eb1e77
Rayhan Mahmud <mahmudrayhan029@gmail.com>
2020-12-30 11:37:11 +0600

    added uppercase and title feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import difflib
-from difflib import get_close_matches 
+from difflib import get_close_matches
 from difflib import SequenceMatcher
 import pyttsx3
 data = json.load(open("data.json"))
@@ -9,26 +9,52 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
+
 def speak(audio):
   engine.say(audio)
   engine.runAndWait()
 
+
+
+
+
+
+
 def query_func(word):
+  word = word.lower()
+ 
   if word in data:
     output = data[word]
     if type(output) == list:
-      for item in output:
-        speak(item)
-        print(item)
+      for items in output:
+        speak(items)
+        return (items)
     else:
       speak(output)
-      print(output)
+      return (output)
+
     
+
+  elif word.upper() in data:        #In case of UPPERCASE words like USA,UK
+    output = data[word.upper()]
+    if type(output) == list:
+      for items in output:
+        speak(items)
+        return (items)
+    else:
+      speak(output)
+      return (output)
+
+  elif word.title() in data:       #In case of words like London, Delhi, Dhaka etc
+    output = data[word.title()]
+    if type(output) == list:
+      for items in output:
+        speak(items)
+        return (items)
+    else:
+      speak(output)
+      return (output)
     
-  
-  elif word.upper() in data:
-    return data[word.upper()]
-  
   elif len(get_close_matches(word, data.keys())) > 0 :
     speak(f"'{word}' is dosen't exists. Do you mean '%s'. Type y if it yes , type n if it no"% get_close_matches(word, data.keys())[0])
     yn = input(f"'{word}' is dosen't exists. Do you mean '%s'. y/n?: "% get_close_matches(word, data.keys())[0])
@@ -83,8 +109,7 @@ while True:
   if word != "0000" and word != "rezwan" and word !=  "rayhan" and word != "mahmud":
     speak("The meanings of the word")
     speak(word)
-  word_capitaliza = word.lower()
-  print(query_func(word_capitaliza))
+  print(query_func(word))
  
 
 
